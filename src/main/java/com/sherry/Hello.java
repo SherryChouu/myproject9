6f7178003452d300224f44d9d4335659953bab70
SherryChouu <sherrychou0710@gmail.com>
2022-06-10 14:55:07 +0800

    Hello world with class Person

--- src/main/java/com/sherry/Hello.java
+++ src/main/java/com/sherry/Hello.java
@@ -1,4 +1,8 @@
 package com.sherry;
 
 public class Hello {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.greeting();
+    }
 }
---
+++ src/main/java/com/sherry/Person.java
@@ -0,0 +1,7 @@
+package com.sherry;
+
+public class Person {
+    public void greeting(){
+        System.out.println("Hello!");
+    }
+}
